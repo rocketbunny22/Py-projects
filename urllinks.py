68f906a6eab5638a98e7e52cc087770c35fc3fbe
Derek Nolt <65988765+rocketbunny22@users.noreply.github.com>
2024-06-20 15:43:16 -0400

    Update urllinks.py

--- urllinks.py
+++ urllinks.py
@@ -1,21 +1,33 @@
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+from urllib.request import Request, urlopen
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read()
+req = Request(
+    url=input("Enter URL: (e.g. https://google.com)  \n"),
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+html = urllib.request.urlopen(req, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
 tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+links = [tag.get('href', None) for tag in tags if tag.get('href', None)]
+
+if links:
+    # Create a dynamic filename using the current timestamp
+    filename = "pahttpge-links.txt"
+    
+    # Write the links to the file
+    with open(filename, "w") as file:
+        for link in links:
+            file.write(link + "\n")
+    
+    print(f"Links saved to {filename}")
+else:
+    print('No links found')
